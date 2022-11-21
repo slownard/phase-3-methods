532b6c33ef5b0fd3468aacae6d7ed9bc9176f438
Bernard Legaspi <blegaspi26@gmail.com>
2022-11-21 16:40:07 -0500

    commit tree 4b8960eff64787ebc37d0bc135ff2b965f5226ef

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer()
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+greet()
+
+def add(num1, num2)
+    puts num1 + num2;
+end 
+add(2, 10)
+
+def halve(num) 
+    if num.class != "number"
+        return nil
+    end
+    num / 2
+end
